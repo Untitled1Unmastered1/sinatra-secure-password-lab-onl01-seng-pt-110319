415628facf048038f814f35bbe3fd381312a750c
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-17 04:21:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,6 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    @user = User.new(:username => params[:username], :password => params[:password])
-    if user.save
-      redirect "/login"
-    else
-      redirect "/failure"
-    end
   end
 
   end
@@ -38,14 +32,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find_by(:username => params[:username])
- 
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect to "/success"
-    else
-      redirect to "/failure"
-    end
+   
   end
   
   get "/failure" do
