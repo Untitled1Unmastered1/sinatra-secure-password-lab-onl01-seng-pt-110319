6a70e8cbb7cac3affbd29de21d648abc9e7757f8
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-17 04:46:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
+    if params[:username]=="" || params[:password]==""
+      redirect to '/failure'
+    end
     user = User.new(:username => params[:username], :password => params[:password])
  
     if user.save
