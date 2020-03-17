7f12276c919721cba69453287b0397a6fc464866
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-17 04:55:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    if params ==""
+    if params[:username]=="" || params[:password]==""
       redirect "/failure"
     else 
       erb :account 
